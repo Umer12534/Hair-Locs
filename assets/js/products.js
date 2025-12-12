bd562a71bb1dbdd0e140f7c9ebccb6c5263db138
Syed Umer <133738875+Umer12534@users.noreply.github.com>
2025-12-12 12:01:13 +0500

    delet unuse file

--- assets/js/products.js
+++
@@ -1,7 +0,0 @@
-function togglefilter() {
-  const filterSidebar = document.getElementById("filter-sidebar");
-  const overlay = document.getElementById("overlay");
-
-  filterSidebar.classList.toggle("active");
-  overlay.classList.toggle("active");
-}
